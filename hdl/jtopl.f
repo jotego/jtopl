a6b845048ef45c191412dbb2d538a3754f9d273f
Jose Tejada <jotego@gmail.com>
2020-06-20 16:55:30 +0200

    Basic FM implemented

--- hdl/jtopl.f
+++ hdl/jtopl.f
@@ -1,3 +1,4 @@
+jtopl_acc.v
 jtopl_csr.v
 jtopl_div.v
 jtopl_eg_cnt.v
@@ -7,8 +8,10 @@ jtopl_eg_final.v
 jtopl_eg_pure.v
 jtopl_eg_step.v
 jtopl_eg.v
+jtopl_exprom.v
 jtopl_logsin.v
 jtopl_mmr.v
+jtopl_op.v
 jtopl_pg_comb.v
 jtopl_pg_inc.v
 jtopl_pg_sum.v
@@ -16,5 +19,6 @@ jtopl_pg.v
 jtopl_reg.v
 jtopl_sh_rst.v
 jtopl_sh.v
+jtopl_single_acc.v
 jtopl_timers.v
 jtopl.v
