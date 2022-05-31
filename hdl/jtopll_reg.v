72549d41632c3303bb7e74d8e925d14742721399
Jose Tejada <jotego@gmail.com>
2022-05-31 22:01:40 +0200

    Fixes missing patch in synthesis

--- hdl/jtopll_reg.v
+++ hdl/jtopll_reg.v
@@ -105,33 +105,34 @@ jtopl_slot_cnt u_slot_cnt(
     .slot   ( slot      )    // hot one encoding of active slot
 );
 
-initial begin
-    patch[01]=64'h170078D0171E6171;
-    patch[02]=64'h1323F7D80D1A4113;
-    patch[03]=64'h2311C4F200990113;
-    patch[04]=64'h277064A8070E6131;
-    patch[05]=64'h280076E0061E2132;
-    patch[06]=64'h180071E005162231;
-    patch[07]=64'h07108182071D6121;
-    patch[08]=64'h070072A2142D2123;
-    patch[09]=64'h17106564061B6161;
-    patch[10]=64'h0771F785180B6141;
-    patch[11]=64'h0410E4FA11830113;
-    patch[12]=64'h1222F8F80724C117;
-    patch[13]=64'h4220F5C2050C5061;
-    patch[14]=64'h020395C903550101;
-    patch[15]=64'h1340E4F103894161;
-    patch[16]=64'h006A00DF0F180001;
-    patch[17]=64'h00A700C800000001;
-    patch[18]=64'h005900F800000005;
-    patch[19]=64'h6D00F80000000100;
-    patch[20]=64'h4800D80000000100;
-    patch[21]=64'h5500AA0000000100;
-end
-
-always @(posedge clk) begin
-    if( up_original ) begin
-        patch[0][ {sel_sub,3'd0} +: 8 ] <= din;
+always @(posedge clk, posedge rst) begin
+    if( rst ) begin
+        patch[00]<=64'h0000000000000000;
+        patch[01]<=64'h170078D0171E6171;
+        patch[02]<=64'h1323F7D80D1A4113;
+        patch[03]<=64'h2311C4F200990113;
+        patch[04]<=64'h277064A8070E6131;
+        patch[05]<=64'h280076E0061E2132;
+        patch[06]<=64'h180071E005162231;
+        patch[07]<=64'h07108182071D6121;
+        patch[08]<=64'h070072A2142D2123;
+        patch[09]<=64'h17106564061B6161;
+        patch[10]<=64'h0771F785180B6141;
+        patch[11]<=64'h0410E4FA11830113;
+        patch[12]<=64'h1222F8F80724C117;
+        patch[13]<=64'h4220F5C2050C5061;
+        patch[14]<=64'h020395C903550101;
+        patch[15]<=64'h1340E4F103894161;
+        patch[16]<=64'h006A00DF0F180001;
+        patch[17]<=64'h00A700C800000001;
+        patch[18]<=64'h005900F800000005;
+        patch[19]<=64'h6D00F80000000100;
+        patch[20]<=64'h4800D80000000100;
+        patch[21]<=64'h5500AA0000000100;
+    end else begin
+        if( up_original ) begin
+            patch[0][ {sel_sub,3'd0} +: 8 ] <= din;
+        end
     end
 end
 
