2829f61ceb26349b5682235ab8712cd1d6b4a53f
Jose Tejada <jose.tejada@jotego.es>
2024-03-23 10:30:37 +0100

    rhythm 2x gain reapplied

--- hdl/jtopl_acc.v
+++ hdl/jtopl_acc.v
@@ -24,6 +24,8 @@ module jtopl_acc(
     input                rst,
     input                clk,
     input                cenop,
+    input         [17:0] slot,
+    input                rhy_en,
     input  signed [12:0] op_result,
     input                zero,
     input                op,  // 0 for modulator operators
@@ -31,15 +33,22 @@ module jtopl_acc(
     output signed [15:0] snd
 );
 
-wire sum_en;
+wire               sum_en;
+wire signed [12:0] op2x;
+wire               rhy2x;
 
+// all rhythm channels are amplified by two
+// given the data path latency, slot 16(-1) data enters at slot 6(-1) and so on
+// slots 13~18 (counting from 1 to 18) will enter when bits slot[7:2] are set
+assign rhy2x  = rhy_en && |slot[7:2];
 assign sum_en = op | con;
+assign op2x   = rhy2x ? op_result<<1 : op_result;
 
 // Continuous output
 jtopl_single_acc  u_acc(
     .clk        ( clk       ),
     .cenop      ( cenop     ),
-    .op_result  ( op_result ),
+    .op_result  ( op2x      ),
     .sum_en     ( sum_en    ),
     .zero       ( zero      ),
     .snd        ( snd       )
